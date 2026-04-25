2b52ec282dc1a541f4abaf496c9dd5be8eb77fe9
utsavg05 <utsav.gp1204@gmail.com>
2026-04-26 01:47:11 +0530

    added cd pipeline and eslint

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe("CICD is wrong!");
+    expect(res.body.message).toBe("CICD is running!");
   });
 });
